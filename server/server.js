a2af21c40bdcc849f0748e2261acbe7f92cb4baf
BaeBischops <dr.bkzee@gmail.com>
2024-03-13 12:03:28 +0200

    instance of openai

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import { express } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import {configuration, OpenAI} from "openai";
+import {configuration, OpenAIApi} from "openai";
 
 dotenv.config();
 
 const configuration = new configuration({
     apikey: process.env.OPENAI_API_KEY,
 });
+
+//instance of openai
+const openai = new OpenAIApi(configuration);
