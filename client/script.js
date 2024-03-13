6b534e56d668f0f3e08fb4e530a9b1a3dc45d81a
BaeBischops <dr.bkzee@gmail.com>
2024-03-13 11:33:48 +0200

    updating the handle submit fxxn

--- client/script.js
+++ client/script.js
@@ -1,87 +1,121 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
-let loadInterval;
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-//the loading function
-const loader = () => {
-  element.textContent = '';
-  
-  loadInterval = setInterval(() => {
-    element.textContent += '.';
+let loadInterval
 
-    if (element.textContent === '....')
-    {
-      element.textContent = '';
-    }
-  }, 300)
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
 
-//the typing text fxn 
-const typeTxt = () => {
-  let index = 0;
-  
-  let interval = setInterval(() => {
-    if (index < text.length)
-    {
-      element.innerHTML += text.charAt(index);
-      index ++;
-    }
-  }, 20)
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
 }
 
-//unique id for every message
-const generateUniqueId = () => {
-  const timeStamp = Date.now();
-  const randomNumber = Math.random();
-  const hexidecimalString = randomNumber.toString(16);
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timeStamp}-${hexidecimalString}`;
+    return `id-${timestamp}-${hexadecimalString}`;
 }
 
-//chat strip fxn
-const chatStrip = (isAi, value, uniqueId) => {
-  return 
-  (
-    `
-    <div class="wrapper ${isAi && 'ai'}">
-      <div class=chat">
-        <div class="profile">
-          <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-        <div class="message" id="${uniqueId}">${value}</div>
-      </div>
-    </div>
     `
-    );
+    )
 }
 
-//asynchronous handle submit fxn
-const handleSubmit = async(e) => {
-  e.preventDefault();
-  
-  const data = new FormData(form);
 
-  chatContainer.innerHTML += chatStrip(false, data.get('prompt'));
-  
-  form.reset();
+const handleSubmit = async (e) => {
+    e.preventDefault()
 
-  const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStrip(true, " ", uniqueId);
+    const data = new FormData(form)
 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-  const messageDiv = document.getElementById(uniqueId);
+    // to clear the textarea input 
+    form.reset()
 
-  loader(messageDiv);
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('https://codex-im0y.onrender.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13)
-  {
-    handleSubmit(e); 
-  }
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
 })
